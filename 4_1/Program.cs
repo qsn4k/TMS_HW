94b0a74dd0502a006876b356f13b7f502d9ce81a
Daniil <denisenkodaniilden@gmail.com>
2025-03-13 18:51:30 +0300

    first

---
+++ 4_1/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Введите количество студентов: ");
+int length = int.Parse(Console.ReadLine());
+int[] arrayStudent = new int[length];
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine($"Введите оценку студента номер {i + 1}:");
+    arrayStudent[i] = int.Parse(Console.ReadLine());
+}
+while (true)
+{
+    Console.WriteLine("Меню");
+    Console.WriteLine("1.Средняя оценка группы");
+    Console.WriteLine("2.Самая высокая и низка оценка группы");
+    Console.WriteLine("3.Количество студентов");
+    Console.WriteLine("0.Выход");
+    Console.WriteLine("Введите: ");
+    int choice = int.Parse(Console.ReadLine());
+    switch (choice)
+    {
+        case 1:
+            int temp = 0;
+            for (int i = 0; i < length; i++)
+            {
+                temp += arrayStudent[i];
+            }
+            double result = temp / (double)length;
+            Console.WriteLine($"Средняя оценка группы = {result}");
+            break;
+        case 2:
+            int min = arrayStudent[0];
+            int max = arrayStudent[0];
+            for (int i = 0; i < length; i++)
+            {
+                if (arrayStudent[i] < min)
+                {
+                    min = arrayStudent[i];
+                }
+                if (arrayStudent[i] > max)
+                {
+                    max = arrayStudent[i];
+                }
+            }
+            Console.WriteLine($"Самая высокая оценка: {max}");
+            Console.WriteLine($"Самая низкая оценка: {min}");
+            break;
+        case 3:
+            Console.WriteLine($"Количество студентов = {length}");
+            break;
+        case 0:
+            return 0;
+        default:
+            Console.WriteLine("Введено неправильное значение");
+            break;
+    }
+}
---
+++ 4_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите размерность массива (например 4 => 4*4): ");
+int size = int.Parse(Console.ReadLine());
+int[,] matrix = new int[size, size];
+for (int rows = 0; rows < size; rows++)
+{
+    for (int col = 0; col < size; col++)
+    {
+        matrix[rows, col] = Random.Shared.Next(-99, 100);
+    }
+}
+for (int rows = 0; rows < size; rows++)
+{
+    for (int col = 0; col < size; col++)
+    {
+        Console.WriteLine(matrix[rows, col]);
+    }
+    Console.WriteLine("");
+}
