023e21d7d57dada7562b377f348234c1aed3fc0b
Daniil <denisenkodaniilden@gmail.com>
2025-03-20 18:59:33 +0300

    1

---
+++ lesson_6/CreditCard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_6
+{
+    class CreditCard
+    {
+        private string number;
+        private int sum;
+
+        CreditCard()
+        {
+            Console.Write("Введите номер счёта:");
+            number = Console.ReadLine();
+            Console.Write("Введите начальную сумма:");
+            while(true)
+            {
+                bool isValid = int.TryParse(Console.ReadLine(), out int sum);
+                if (!isValid) break;
+                Console.Write("Введите ещё раз:");
+            }
+        }
+
+        CreditCard(string number, int sum)
+        {
+            this.number = number;
+            this.sum = sum;
+        }
+        int editSum(int sum)
+        {
+            return this.sum += sum;
+        }
+
+    }
+}
