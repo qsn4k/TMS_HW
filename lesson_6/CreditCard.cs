638a631e34813f0b13c74b91a79f602a9c1076a8
Daniil <denisenkodaniilden@gmail.com>
2025-03-21 10:39:54 +0300

    ready

--- lesson_6/CreditCard.cs
+++ lesson_6/CreditCard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,31 +9,76 @@ namespace lesson_6
 {
     class CreditCard
     {
-        private string number;
-        private int sum;
+        private string number = "";
+        private int sum = 0;
 
-        CreditCard()
+        public CreditCard()
         {
             Console.Write("Введите номер счёта:");
             number = Console.ReadLine();
             Console.Write("Введите начальную сумма:");
-            while(true)
+            while (true)
             {
-                bool isValid = int.TryParse(Console.ReadLine(), out int sum);
-                if (!isValid) break;
+                bool isValid = int.TryParse(Console.ReadLine(), out sum);
+                if (isValid) break;
                 Console.Write("Введите ещё раз:");
             }
         }
 
-        CreditCard(string number, int sum)
+        public CreditCard(string number, int sum)
         {
             this.number = number;
             this.sum = sum;
         }
-        int editSum(int sum)
+
+        public void PutMoney(int s)
         {
-            return this.sum += sum;
+            if (s >= 0) sum += s;
         }
 
+        public void PutMoney()
+        {
+            Console.Write("Какую сумму вы хотите положить: ");
+            while (true)
+            {
+                bool isValid = int.TryParse(Console.ReadLine(), out int money);
+                if (isValid)
+                {
+                    if (money >= 0) sum += money;
+                    break;
+                }
+                Console.Write("Вы ввели неправильное число. Введите ещё раз:");
+            }
+        }
+
+        public void TakeMoney(int s)
+        {
+            if (sum - s >= 0) sum -= s;
+            else Console.WriteLine("Недостаточно средств");
+        }
+
+        public void TakeMoney()
+        {
+            Console.Write("Введите сумму, которую желаете снять: ");
+            while (true)
+            {
+                bool isValid = int.TryParse(Console.ReadLine(), out int money);
+                if (isValid)
+                {
+                    if (sum - money >= 0) sum -= money;
+                    break;
+                }
+                Console.Write("Вы ввели неправильное число. Введите ещё раз:");
+            }
+        }
+
+        public void Information()
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine($"Информация о карточке:");
+            Console.WriteLine($"Номер счёта: {number}");
+            Console.WriteLine($"Сумма на счёте: {sum}");
+            Console.WriteLine("-----------------------");
+        }
     }
 }
--- lesson_6/Program.cs
+++ lesson_6/Program.cs
@@ -2,9 +2,98 @@
 {
     internal class Program
     {
-        static void Main()
+        static int Main()
         {
+            Console.WriteLine("Заполните первоначальные данные для счетов: ");
+            CreditCard[] creditCards = new CreditCard[3];
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Счёт {i+1}:");
+                creditCards[i] = new CreditCard();
+            }
+            
+            while (true)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1.Положить деньги на счёт");
+                Console.WriteLine("2.Снять деньги со счёта");
+                Console.WriteLine("3.Получить информацию о карте");
+                Console.WriteLine("4.Получить информацию о всех картах");
+                Console.WriteLine("0.Выход");
+                int choice = 0;
+                while (true)
+                {
+                    bool isValid = int.TryParse(Console.ReadLine(), out choice);
+                    if(isValid)
+                    {
+                        if (choice >= 0 && choice <= 4)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.Write("Вы ввели не правильный номер действия. ПОпробуйте снова: ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("Вы ввели не число. Попробуйте снова: ");
+                    }
+                }
 
+                int result = 0;
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Введите порядковый номер счёта: ");
+                        while (true)
+                        {
+                            bool isValid = int.TryParse(Console.ReadLine(), out result);
+                            if (isValid) if (result > 0 && result < 4) break;
+                            Console.Write("Не правильное значение. Повторите попытку: ");
+                        }
+                        result--;
+                        creditCards[result].PutMoney();
+                        break;
+                    case 2:
+                        Console.Write("Введите порядковый номер счёта: ");
+                        while (true)
+                        {
+                            bool isValid = int.TryParse(Console.ReadLine(), out result);
+                            if (isValid) if (result > 0 && result < 4) break;
+                            Console.Write("Не правильное значение. Повторите попытку: ");
+                        }
+                        result--;
+                        creditCards[result].TakeMoney();
+                        break;
+                    case 3:
+                        Console.Write("Введите порядковый номер счёта: ");
+                        while (true)
+                        {
+                            bool isValid = int.TryParse(Console.ReadLine(), out result);
+                            if (isValid) if (result > 0 && result < 4) break;
+                            Console.Write("Не правильное значение. Повторите попытку: ");
+                        }
+                        result--;
+                        creditCards[result].Information();
+                        break;
+                    case 4:
+                        for(int i = 0; i < 3; i++)
+                        {
+                            Console.WriteLine($"\nСчёт {i+1}:");
+                            creditCards[i].Information();
+                        }
+                        break;
+                    case 0:
+                        return 0;
+                    default:
+                        break;
+                }
+            }
         }
+
+
+
     }
 }
