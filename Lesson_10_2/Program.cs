7df0584b043091548b35fefe9d86a8bcb100f916
Daniil <denisenkodaniilden@gmail.com>
2025-04-04 11:16:31 +0300

    plack plack

---
+++ Lesson_10_2/Program.cs
@@ -0,0 +1,48 @@
+﻿//2. Написать программу, которая уведомляет менеджера и клиента о статусе заказа с помощью событий:
+//    -Создать класс OnlineStore с методом обработки заказа (ProccessOrder(int orderId)) и событием OnOrderProcessed
+//    - Событие должно срабатывать при обработке заказа
+//    - Добавить обработчики события:
+//        1) Уведомление клиенту(Выводит в консоль "Уведомление клиенту: Ваш заказ {id} готов!")
+//        2) Уведомление менеджеру(Выводит в консоль "Уведомление менеджеру: Заказ {id} обработан.")
+//    -Обработать несколько заказов, проверяя, что события вызываются автоматически
+
+namespace Lesson_10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<OnlineStore> onlineStores = new List<OnlineStore> {new OnlineStore(), new OnlineStore() };
+            onlineStores[0].OnOrderProccess += OnlineStore.MessageClient;
+            onlineStores[0].OnOrderProccess += OnlineStore.MessageManager;
+
+            onlineStores[0].ProccessOrder(10);
+            onlineStores[0].ProccessOrder(20);
+            onlineStores[1].ProccessOrder(11);
+            onlineStores[1].ProccessOrder(21);
+            onlineStores[0].ProccessOrder(5430);
+        }
+    }
+
+    class OnlineStore
+    {
+
+        public event Action<int> OnOrderProccess;
+
+        public void ProccessOrder (int orderId)
+        {
+            OnOrderProccess?.Invoke(orderId);
+        }
+
+        static public void MessageClient(int orderId)
+        {
+            Console.WriteLine($"Уведомление клиенту: Ваш заказ {orderId} готов!");
+        }
+
+        static public void MessageManager(int orderId)
+        {
+            Console.WriteLine($"Уведомление менеджеру: Заказ {orderId} обработан.");
+        }
+    }
+}
---
+++ lesson_10/Program.cs
@@ -0,0 +1,64 @@
+﻿//1.Написать программу, которая использует делегаты для фильтрации списка чисел:
+//    -Создать список чисел и вывести на экран
+//    - Предложить пользователю выбрать способ фильтрации (оставить только чётные числа, только нечётные, числа больше 10 и т. п.)
+//    - Реализовать метод Filter, который принимает список чисел и делегат-фильтр и возвращает отфильтрованный список
+//    - Отфильтровать список и вывести на экран результат
+
+
+using System.Dynamic;
+
+namespace lesson_10
+{
+    internal class Program
+    {
+        //Predicate<int, int> finc = (int a, int b) a > b => false;
+
+
+
+        static void Main(string[] args)
+        {
+            List<int> listInt = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.Write("Список чисел:");
+            foreach (var item in listInt)
+            {
+                Console.Write(string.Join(" ", $", {item}"));
+            }
+            Console.WriteLine("Способ фильтрации: " +
+                "\n1. Оставить четные числа" +
+                "\n2. Оставить нечетные числа" +
+                "\n3. Оставить только те, которые делятся на 3");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    listInt = Filter((int a) => a % 2 == 0 ? true : false, listInt);
+                    break;
+                case 2:
+                    listInt = Filter((int a) => a % 2 == 1 ? true : false, listInt);
+                    break;
+                case 3:
+                    listInt = Filter((int a) => a % 3 == 0 ? true : false, listInt);
+                    break;
+                default:
+                    break;
+            }
+
+            Console.Write("Список чисел:");
+            foreach (var item in listInt)
+            {
+                Console.Write(string.Join(" ", $", {item}"));
+            }
+        }
+
+        static List<int> Filter(Predicate<int> predicate, List<int> list)
+        {
+            List<int> resultList = new();
+            foreach (var a in list)
+            {
+                if (predicate(a)) resultList.Add(a);
+            }
+            return resultList;
+        }
+
+    }
+}
