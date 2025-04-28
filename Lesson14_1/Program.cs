0d269dbaf61c7079491a091095992638ff8f3bce
Daniil <denisenkodaniilden@gmail.com>
2025-04-28 14:18:50 +0300

    1

---
+++ Lesson14_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json;
+
+namespace Lesson14_1
+{
+    internal class Program
+    {
+        class User
+        {
+            public int Id { get; set; }
+            
+            public string Name { get; set; }
+
+            public string Email { get; set; }
+
+            public Address Address { get; set; }
+
+            public string Phone { get; set; }
+
+            public string Websity { get; set; }
+
+            public Company Company { get; set; }
+        }
+
+        class Address
+        {
+            public string Street { get; set; }
+
+            public string Suite { get; set; }
+            
+            public string City { get; set; }
+
+            public string Zipcode { get; set; }
+
+            public struct Geo
+            {
+                decimal Lat { get; set; }
+                
+                decimal Lng { get; set; }
+            }
+        }
+
+        class Company
+        {
+            public string Name { get; set; }
+
+            public string CatchPhrase { get; set; }
+
+            public string Bs { get; set; }
+        }
+
+        static async Task Main()
+        {
+            HttpClient client = new HttpClient();
+            var user = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+            var content = await user.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+
+        }
+    }
+}
