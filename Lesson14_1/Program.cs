095e1c6fb1f786601aac1d99430483f663f90c42
Daniil <denisenkodaniilden@gmail.com>
2025-04-28 17:14:36 +0300

    1

--- Lesson14_1/Program.cs
+++ Lesson14_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Lesson14_1
 {
@@ -6,46 +7,73 @@ namespace Lesson14_1
     {
         class User
         {
-            public int Id { get; set; }
-            
-            public string Name { get; set; }
+            [JsonPropertyName("id")]
+            public int Id { get; set; } = 0;
 
-            public string Email { get; set; }
+            [JsonPropertyName("name")]
+            public string Name { get; set; } = "";
 
+            [JsonPropertyName("email")]
+            public string Email { get; set; } = "";
+
+            [JsonPropertyName("address")]
             public Address Address { get; set; }
 
-            public string Phone { get; set; }
+            [JsonPropertyName("phone")]
+            public string Phone { get; set; } = "";
 
-            public string Websity { get; set; }
+            [JsonPropertyName("websity")]
+            public string Websity { get; set; } = "";
 
+            [JsonPropertyName("company")]
             public Company Company { get; set; }
+
+            public void WriteData()
+            {
+                Console.WriteLine($"Name: \t{Name}");
+                Console.WriteLine($"Email: \t{Email}");
+                Console.WriteLine($"Name: \t{Company.Name}");
+            }
+
         }
 
         class Address
         {
-            public string Street { get; set; }
+            [JsonPropertyName("street")]
+            public string Street { get; set; } = "";
 
-            public string Suite { get; set; }
-            
-            public string City { get; set; }
+            [JsonPropertyName("suite")]
+            public string Suite { get; set; } = "";
 
-            public string Zipcode { get; set; }
+            [JsonPropertyName("city")]
+            public string City { get; set; } = "";
 
-            public struct Geo
-            {
-                decimal Lat { get; set; }
-                
-                decimal Lng { get; set; }
-            }
+            [JsonPropertyName("zipcode")]
+            public string Zipcode { get; set; } = "";
+
+            [JsonPropertyName("geo")]
+            public Geo Geo { get; set; }
+
+        }
+        class Geo
+        {
+            [JsonPropertyName("lat")]
+            string Lat { get; set; } = "0";
+
+            [JsonPropertyName("lng")]
+            string Lng { get; set; } = "0";
         }
 
         class Company
         {
-            public string Name { get; set; }
+            [JsonPropertyName("name")]
+            public string Name { get; set; } = "";
 
-            public string CatchPhrase { get; set; }
+            [JsonPropertyName("catchPhrase")]
+            public string CatchPhrase { get; set; } = "";
 
-            public string Bs { get; set; }
+            [JsonPropertyName("bs")]
+            public string Bs { get; set; } = "";
         }
 
         static async Task Main()
@@ -53,8 +81,26 @@ namespace Lesson14_1
             HttpClient client = new HttpClient();
             var user = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
             var content = await user.Content.ReadAsStringAsync();
-            Console.WriteLine(content);
 
+            List<User> users = JsonSerializer.Deserialize<List<User>>(content);
+
+            foreach(var item in users)
+            {
+                Console.WriteLine("-----");
+                item.WriteData();
+                Console.WriteLine("-----");
+            }
+            Console.WriteLine("");
+
+            User newUser = new User();
+            newUser.Name = "Daniil Petrovich";
+            newUser.Email = "dan10Pet@gmail.com";
+            //newUser.Company.Name = "Google";
+
+            var contentUser = JsonSerializer.Serialize<User>(newUser);
+            var contentHttp = new StringContent(contentUser);
+            var codePost = await client.PostAsync("https://jsonplaceholder.typicode.com/users", contentHttp);
+            Console.WriteLine($"{codePost.StatusCode} || {(int)codePost.StatusCode}");
         }
     }
 }
