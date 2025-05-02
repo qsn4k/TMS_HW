6e4af92f8b9dd76d7b5a334c746669bcc961b161
Daniil <denisenkodaniilden@gmail.com>
2025-05-02 09:19:54 +0300

    1

---
+++ lesson_15/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace lesson_15.Models
+{
+    public class Student
+    {
+        private string Name { get; set; }
+
+        private string Group { get; set; }
+
+        private string Speciality { get; set; }
+
+
+
+        public Student(string name, string group, string speciality)
+        {
+            Name = name;
+            Group = group;
+            Speciality = speciality;
+        }
+    }
+}
