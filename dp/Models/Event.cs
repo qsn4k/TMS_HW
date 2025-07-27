f17a96bacaff827c341510d5973021654ea8443b
Daniil <denisenkodaniilden@gmail.com>
2025-07-27 18:30:31 +0300

    1

---
+++ dp/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace dp.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Location { get; set; }
+
+        public int TicketCount { get; set; }
+
+        public List<Ticket> Tickets { get; set; }
+    }
+}
---
+++ dp/Models/Ticket.cs
@@ -0,0 +1,20 @@
+﻿namespace dp.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        public int EventId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Status { get; set; }
+
+        public int Row { get; set; }
+
+        public int Place { get; set; }
+
+        public bool PaymentStatus { get; set; }
+
+    }
+}
