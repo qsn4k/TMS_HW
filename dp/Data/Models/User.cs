303f97f1fdc85d01196956eb116e9e2c2107d473
Daniil <denisenkodaniilden@gmail.com>
2025-08-06 17:39:59 +0300

    0

---
+++ dp/Data/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace dp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string Role { get; set; }
+
+        public struct Basket
+        {
+            public List<Ticket> UsedTickets { get; set; }
+
+            public List<Ticket> Tickets { get; set; }
+        }
+    }
+}
